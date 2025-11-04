8f40e267684ab248b46ffe5de6ecdd735ed1e100
lijanahmmed <lijanahmmed@gmail.com>
2025-11-04 23:42:53 +0600

    added product bid modal

--- src/components/ProductDetails/ProductDetails.jsx
+++ src/components/ProductDetails/ProductDetails.jsx
@@ -1,14 +1,122 @@
-import React, { useRef } from 'react';
-import { useLoaderData } from 'react-router';
+import React, { use, useRef } from "react";
+import { useLoaderData } from "react-router";
+import { AuthContext } from "../../contexts/AuthContext";
 
 const ProductDetails = () => {
-    const { _id: productId } = useLoaderData();
-    const bidModalRef = useRef(null);
-    return (
+  const { _id: productId } = useLoaderData();
+  const bidModalRef = useRef(null);
+  const { user } = use(AuthContext);
+
+  const handleBidModalOpen = () => {
+    bidModalRef.current.showModal();
+  };
+
+  const handleBidSubmit = (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    const bid = e.target.bid.value;
+
+    const newBid = {
+      product: productId,
+      buyer_name: name,
+      buyer_email: email,
+      buyer_image: user?.photoURL,
+      bid_price: bid,
+      status: "pending",
+    };
+
+    fetch("http://localhost:3000/bids", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(newBid),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        // if (data.insertedId) {
+        //   bidModalRef.current.close();
+        //   Swal.fire({
+        //     position: "top-end",
+        //     icon: "success",
+        //     title: "Your bid has been placed.",
+        //     showConfirmButton: false,
+        //     timer: 1500,
+        //   });
+        //   // add the new bid to the state
+        //   newBid._id = data.insertedId;
+        //   const newBids = [...bids, newBid];
+        //   newBids.sort((a, b) => b.bid_price - a.bid_price);
+        //   setBids(newBids);
+        // }
+      });
+  };
+
+  return (
+    <div>
+      <div>
+        <div></div>
         <div>
-            
+          <button onClick={handleBidModalOpen} className="btn btn-primary">
+            I want to Buy this Product
+          </button>
+
+          <dialog
+            ref={bidModalRef}
+            className="modal modal-bottom sm:modal-middle"
+          >
+            <div className="modal-box">
+              <h3 className="font-bold text-lg">Give the best offer!</h3>
+              <p className="py-4">Offer something seller can not resist</p>
+              <form onSubmit={handleBidSubmit}>
+                <fieldset className="fieldset">
+                  <label className="label">Name</label>
+                  <input
+                    type="text"
+                    name="name"
+                    className="input"
+                    readOnly
+                    defaultValue={user?.displayName}
+                  />
+                  {/* email */}
+                  <label className="label">Email</label>
+                  <input
+                    type="email"
+                    className="input"
+                    name="email"
+                    readOnly
+                    defaultValue={user?.email}
+                  />
+                  {/* bid amount */}
+                  <label className="label">Bid</label>
+                  <input
+                    type="text"
+                    name="bid"
+                    className="input"
+                    placeholder="Your Bid"
+                  />
+                  <button className="btn btn-neutral mt-4">
+                    Please your bid
+                  </button>
+                </fieldset>
+              </form>
+
+              <div className="modal-action">
+                <form method="dialog">
+                  {/* if there is a button in form, it will close the modal */}
+                  <button className="btn">Cancel</button>
+                </form>
+              </div>
+            </div>
+          </dialog>
         </div>
-    );
+      </div>
+      {/* bids for this product */}
+      <div></div>
+    </div>
+  );
 };
 
 export default ProductDetails;
